ca8d7d4499b4eabf49898bef434e8f545bb634bf
dsomuncuoglu <dsomuncuoglu@gmail.com>
2022-08-29 11:05:56 +0300

    variables

---
+++ variables.py
@@ -0,0 +1,9 @@
+maasAli=5000
+maasAhmet=4000
+vergi=0.27
+
+netmaasAli=maasAli-(maasAli*vergi)
+netmaasAhmet=maasAhmet-(maasAhmet*vergi)
+
+print("Ali'nin net maasi="+str(netmaasAli))
+print("Ahmet'in net maasi="+str(netmaasAhmet))
