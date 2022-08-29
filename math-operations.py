6ccad892d21213e5136bc5cfa9ee96692c448706
dsomuncuoglu <dsomuncuoglu@gmail.com>
2022-08-29 10:26:40 +0300

    uptdate_1

---
+++ math-operations.py
@@ -0,0 +1,12 @@
+#*
+# Toplama (+)
+# Cikarma (-)
+# Carpma (*)
+# Bolme (/)
+# Us (**)
+# Mod (%)
+# Tam Bolme (//)
+# *#
+
+print(4*2)
+print(2**4)
---
+++ numbers.py
@@ -0,0 +1,12 @@
+#Sayi veri tipleri
+#İnteger 5
+#Float 5.52
+
+a=2+2
+b=2.2+5.1
+print("a=")
+print(a)
+print(type(a))
+print("b=")
+print(b)
+print(type(b))
