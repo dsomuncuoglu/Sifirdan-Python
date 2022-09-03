c7c766dae0543a987848b5de1da5e6240cce1f55
dsomuncuoglu <dsomuncuoglu@gmail.com>
2022-09-03 23:58:47 +0300

    Set

---
+++ sets.py
@@ -0,0 +1,36 @@
+fruits={"apple","banana","orange","dragon fruit","peace","melon"}
+
+# print(fruits[0]) indexlenemez sets
+
+global x
+def printFruits(): # Ekrana yazdırma fonksiyonu
+    x=1
+    for i in fruits:
+        print(str(x)+".Meyve => "+str(i))
+        x+=1
+
+
+# Ekleme işlemi
+fruits.add("raspberry")
+fruits.update(["mango","grape"]) # Dizi atama
+
+printFruits() # Fonksiyon çağırma
+print("\n")
+
+# Silme İşlemi
+fruits.remove("dragon fruit")
+fruits.discard("melon")
+
+printFruits()
+print("\n")
+
+fruits.pop() # Set'de sıra olmadığı için pop ile rastgele bir eleman silinebilir.
+
+printFruits()
+print("\n")
+
+expList=[1,2,3,4,4,5,6,5,6,8]
+
+print(expList) # Normal liste
+print(set(expList)) # Set'in en önemli yanı tekrar eden verileri almaz.
+
