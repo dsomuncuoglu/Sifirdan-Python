db129b28050e5338135fdc5c42fe7f4c633a822c
dsomuncuoglu <dsomuncuoglu@gmail.com>
2022-09-05 08:52:55 +0300

    Atama Operatörleri

---
+++ assignment.py
@@ -0,0 +1,26 @@
+# x=5
+# y=10
+# z=15
+
+x,y,z,=5,10,15 # 3 satırda yapılan atama işlemi tek satıra indirilmiştir.
+
+x,y=y,x # x ve y nin değerleri değiştirilmiştir.
+
+x=x+5 # veya x+=5 aynı işlemi yapar.
+x=x-5 # veya x-=5 aynı işlemi yapar.
+x=x*5 # veya x*=5 aynı işlemi yapar.
+x=x/5 # veya x/=5 aynı işlemi yapar.
+x=x%5 # veya x%=5 aynı işlemi yapar.
+x=x//5 # veya x//=5 aynı işlemi yapar.
+x=x**5 # veya x**=5 aynı işlemi yapar.
+
+print(x,y,z)
+
+
+values=1,2,3
+
+print(values)
+print(type(values))
+
+x,y,z=values
+print(x,y,z)
