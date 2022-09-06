cbd3ad50b86dff1b74814f69f3b2ef8b23617082
dsomuncuoglu <dsomuncuoglu@gmail.com>
2022-09-06 17:36:13 +0300

    Selenium Bot

---
+++ Python_Basic/for.py
@@ -0,0 +1,17 @@
+numbers_=[1,2,3,4,5]
+for num in numbers_:
+    print(num)
+
+name="Deniz Somuncuoğlu"
+
+for n in name:
+    print(f"=> {n}")
+
+dic={"dic_1":1,"dic_2":2,"dic_3":3}
+
+for items in dic.items():
+    print(f"Dictionary değeri = {items}")
+
+for key,val in dic.items():
+    print(f"Dictionary key = {key}")
+    print(f"Dictionary value = {val}")
