c64cca051cd2e7463ce2de8fad7a0c2fa3235591
dsomuncuoglu <dsomuncuoglu@gmail.com>
2022-09-09 17:13:58 +0300

    Github Search Text v2

--- Python_Bot/web_selectors.py
+++ Python_Bot/web_selectors.py
@@ -16,9 +16,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium import webdriver
 import time
 
-
 inputText=input("Github'da aramak istediğiniz başlık nedir?\n=>")
-
 
 driver=webdriver.Edge()
 url="https://github.com"
@@ -33,23 +31,14 @@ typeText=driver.find_elements(By.CSS_SELECTOR,".v-align-middle")
 blank=driver.find_elements(By.CSS_SELECTOR,".blankslate")
 bulunamadi=(f"We couldn’t find any repositories matching '{inputText}'\nYou could try an advanced search.")
 
-
-
 for blanks in blank:
     blankText=blanks.text
     if blankText in bulunamadi:
         print(f"Aranan {inputText} söz dizini bulunamadı.")
         break
-    
+
 for element in typeText:
     print(element.text)
-
-
-
-        
-        
-        
-
 
 driver.close()
 
